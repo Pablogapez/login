93a3628a8cd5fcab6484b9132028ceb12d126046
Pablo <pablogapez77@gmail.com>
2022-12-08 23:20:07 +0100

    InicioProyecto

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    //
+    public function show(){
+        return view('auth.register');
+    }
+
+    public function registe(RegisterRequest $request){
+        $user = User::create($request->validated());
+        auth()->login($user);
+        return redirect('/home')->with('success', "Account successfully registered.");
+    }
+}
